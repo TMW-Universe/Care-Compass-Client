0b0d0e708e2ca0a9b475bfaf79c1130eb2e8070b
Joel Campos <joelcamposoliva9000@gmail.com>
2023-08-07 09:43:15 +0200

    Config

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./config/msal/msal.config.ts";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { i18nsetup } from "./i18n/i18n.setup.ts";
-import "./index.css";
+import { ConfigProvider } from "antd";
 import "./reset.css";
+import "./index.css";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 i18nsetup();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <App />
-    </MsalProvider>
+    <ConfigProvider>
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
+    </ConfigProvider>
   </React.StrictMode>
 );
