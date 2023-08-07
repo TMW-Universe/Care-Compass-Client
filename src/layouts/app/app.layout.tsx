ddc225e7ae82925ec94ad2527dc51b18fcfef367
Joel Campos <joelcamposoliva9000@gmail.com>
2023-08-07 11:39:52 +0200

    Nice layout & nesting

--- src/layouts/app/app.layout.tsx
+++ src/layouts/app/app.layout.tsx
@@ -1,7 +1,35 @@
+import { Menu } from "@mui/icons-material";
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import pj from "../../../package.json";
+import styles from "./app.layout.module.css";
+
 type Props = {
   children: JSX.Element;
 };
 
 export default function AppLayout({ children }: Props) {
-  return <>{children}</>;
+  return (
+    <div className={styles.layout}>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <Menu />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {pj.long_name}
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div className={styles.body}>{children}</div>
+    </div>
+  );
 }
