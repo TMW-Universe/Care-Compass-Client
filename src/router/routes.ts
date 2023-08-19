f310b4816f5507192ca1fca01efcf4b4f5b03152
Joel Campos <joelcamposoliva9000@gmail.com>
2023-08-19 11:58:51 +0200

    Remove final slash

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,5 +2,5 @@ type Route = (params: never) => string;
 
 export const routes = {
   MAIN_PAGE: () => "/",
-  WEIGHT_MONITOR: () => "/weight-monitor/",
+  WEIGHT_MONITOR: () => "/weight-monitor",
 } satisfies Record<string, Route>;
